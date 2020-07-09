c99a6e85eebb9ea747e00f6b0b9e6ea42c0c99b5
sapentia <33371127+sapentia@users.noreply.github.com>
2020-07-09 17:44:12 +0100

    Add files via upload

--- psg/models/products.py
+++ psg/models/products.py
@@ -8,11 +8,7 @@ class psg_products(models.Model):
 
     sec_grade = fields.Integer('Security Grade')
     env_class = fields.Char('Environmental Class')
-    applies_to_ids = fields.One2many(
-        comodel_name='psg.applications',
-        inverse_name='product_id',
-        string='Applies To',
-        required=False)
+    applies_to_ids = fields.Char('Applies to')
     install = fields.Float('Installation Faction')
     amps = fields.Float('Amp Rating')
 
@@ -25,7 +21,4 @@ class psg_applications(models.Model):
 
     name = fields.Char('Application')
     active = fields.Boolean('Active')
-    product_id = fields.Many2one(
-        comodel_name='product.template',
-        string='Product_id',
-        required=False)
+    product_id = fields.Char('Product')
